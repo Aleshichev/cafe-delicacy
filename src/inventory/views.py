22490d8199aebfba440dc5f3386177f2b4f98974
Igor <Aleshichev.git@email.ua>
2022-07-08 16:01:57 +0300

    first test app

---
+++ src/inventory/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home(request):
+    context = {"name": "vegetable"}
+    return render(request, "inventory/home.html", context)
