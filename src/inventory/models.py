662fd8e54bac8f7493da54fbb0a7e2c38a49a6de
Igor <Aleshichev.git@email.ua>
2022-07-09 21:26:57 +0300

    second test app

--- src/inventory/models.py
+++ src/inventory/models.py
@@ -6,24 +6,25 @@ from django.db import models
 class Ingredients(models.Model):
   name = models.CharField(max_length=200)
   quantity = models.IntegerField(default=0)
+  unit = models.CharField(max_length=20)
   price = models.IntegerField(default=0)
-
-# A list of the ingredients that each menu item requires (RecipeRequirements)
-
-class RecipeRequirements(models.Model):
-  name = models.CharField(max_length=200)
-  price = models.IntegerField(default=0)
-  ingredients = models.ForeignKey(Ingredients, on_delete=models.CASCADE)
 
 # A list of the restaurant’s MenuItems, and the price set for each entry
 
 class MenuItems(models.Model):
-  name_item = models.CharField(max_length=200)
-  reciperequirements = models.ForeignKey(RecipeRequirements, on_delete=models.CASCADE)
-
-# A log of all Purchases made at the restaurant
-class Purchases(models.Model):
-  name = models.CharField(max_length=200)
-  date = models.DateField()
+  titel = models.CharField(max_length=200)
   price = models.IntegerField(default=0)
-  menuitems = models.ForeignKey(MenuItems, on_delete=models.CASCADE)
+  # reciperequirements = models.ForeignKey(RecipeRequirements, on_delete=models.CASCADE)
+
+# A list of the ingredients that each menu item requires (RecipeRequirements)
+
+class RecipeRequirements(models.Model):
+  menuitem = models.ForeignKey(MenuItems, on_delete=models.CASCADE)
+  ingredient = models.ForeignKey(Ingredients, on_delete=models.CASCADE)
+  quantity = models.IntegerField(default=0)
+# A log of all Purchases made at the restaurant
+
+class Purchases(models.Model):
+  menuitem = models.ForeignKey(MenuItems, on_delete=models.CASCADE)
+  timestamp = models.DateTimeField()
+
